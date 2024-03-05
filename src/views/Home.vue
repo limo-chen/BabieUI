2a2f83ce5315048f27c36ac96976c53613b487b6
Calista <2320035651@qq.com>
2024-03-06 00:40:19 +0800

    topnav封装完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,6 @@
-<!-- import Barbie_Logo from './assets/Barbie_Logo.png' -->
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo">LOGO</div>
-      <div class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </div>
-    </div>
+    <Topnav />
     <div class="banner">
       <h1>BarbieUI</h1>
       <h2>一个厉害的 UI 框架</h2>
@@ -15,22 +8,11 @@
     </div>
   </div>
 </template>
-<style lang="scss" scoped>
-.topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    min-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-}
-</style>
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: {
+    Topnav,
+  },
+};
+</script>
