a7cb3c29167feb5e8d1c9e38ba5934d895cdc27c
Calista <2320035651@qq.com>
2024-03-08 00:26:35 +0800

    锁版本

--- src/lib/SwitchDemo.vue
+++ src/lib/SwitchDemo.vue
@@ -32,24 +32,25 @@ button {
   background: #dcdcdc;
   border-radius: $h/2;
   position: relative;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-  transition: all 250ms;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
 }
 
 button.checked {
   background: #338cff;
+  > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
 }
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
+
 button:active {
   > span {
     width: $h2 + 4px;
