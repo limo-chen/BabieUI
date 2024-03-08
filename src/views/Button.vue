4809674b0efd5213a17a44428b2e4ed6b39c04ce
Calista <2320035651@qq.com>
2024-03-09 01:23:12 +0800

    添加 click 示例

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -2,7 +2,7 @@
   <div>button示例</div>
   <h1>示例1</h1>
   <div>
-    <ButtonDemo>你好</ButtonDemo>
+    <ButtonDemo @click="onClick">你好</ButtonDemo>
     <ButtonDemo theme="button">你好</ButtonDemo>
     <ButtonDemo theme="link">你好</ButtonDemo>
     <ButtonDemo theme="text">你好</ButtonDemo>
