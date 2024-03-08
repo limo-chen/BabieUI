0f0c0aec6a9804a84ada4c84645314c8397fb34d
Calista <2320035651@qq.com>
2024-03-08 17:46:00 +0800

    创建button组件

--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,1 +1,16 @@
-<template><div>hi</div></template>
+<template>
+  <div>button示例</div>
+  <h1>示例1</h1>
+  <div>
+    <ButtonDemo>你好</ButtonDemo>
+  </div>
+</template>
+
+<script lang="ts">
+import ButtonDemo from "../lib/ButtonDemo.vue";
+export default {
+  components: {
+    ButtonDemo,
+  },
+};
+</script>
