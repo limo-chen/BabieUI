6a5067a48cc0d7492fb1ba5a9b8564e5821dacc2
Calista <2320035651@qq.com>
2024-03-11 10:03:23 +0800

    基本样式添加

--- src/lib/DialogDemo.vue
+++ src/lib/DialogDemo.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="dialog-overlay">Dialog组件</div>
-  <div class="dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>第一行字</p>
-      <p>第二行字</p>
-    </main>
-    <footer>
-      <ButtonDemo>OK</ButtonDemo>
-      <ButtonDemo>Cancel</ButtonDemo>
-    </footer>
+  <div class="barbie-dialog-overlay"></div>
+  <div class="barbie-dialog-wrapper">
+    <div class="barbie-dialog">
+      <header>
+        标题
+        <span class="barbie-dialog-close"></span>
+      </header>
+      <main>
+        <p>第一行字</p>
+        <p>第二行字</p>
+      </main>
+      <footer>
+        <ButtonDemo level="main">OK</ButtonDemo>
+        <ButtonDemo>Cancel</ButtonDemo>
+      </footer>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -20,3 +25,74 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+$radius: 4px;
+$boreder-color: #d9d9d9;
+
+.barbie-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+  // 遮罩层
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+  > header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $boreder-color;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 20px;
+  }
+  > main {
+    padding: 12px 16px;
+  }
+  > footer {
+    border-top: 1px solid $boreder-color;
+
+    padding: 12px 16px;
+    text-align: right;
+  }
+  // 关闭按钮
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+    // 将 ::before 和 ::after 伪元素向左和向上移动一定的距离，并且分别将它们逆时针旋转 45 度和顺时针旋转 45 度
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
+</style>
