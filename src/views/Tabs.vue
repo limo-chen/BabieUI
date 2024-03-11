a87346de0ed8d647fbbe4086164cf6d8225e4a1e
Calista <2320035651@qq.com>
2024-03-11 22:12:45 +0800

    创建Tab以及Tabs组件

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,1 +1,18 @@
-<template><div>Tab组件</div></template>
+<template>
+  <div>Tab示例</div>
+  <h1>示例</h1>
+  <TabsDemo>
+    <TabDemo title="导航一">内容一</TabDemo>
+    <TabDemo title="导航二">内容二</TabDemo>
+  </TabsDemo>
+</template>
+<script lang="ts">
+import TabDemo from "../lib/TabDemo.vue";
+import TabsDemo from "../lib/TabsDemo.vue";
+export default {
+  components: {
+    TabDemo,
+    TabsDemo,
+  },
+};
+</script>
