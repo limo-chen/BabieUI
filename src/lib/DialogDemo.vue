8e45ca55c4f68ad56ac436f4816c319a3dd04371
Calista <2320035651@qq.com>
2024-03-11 09:15:32 +0800

    创建DialogDemo组件

---
+++ src/lib/DialogDemo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dialog-overlay">Dialog组件</div>
+  <div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行字</p>
+      <p>第二行字</p>
+    </main>
+    <footer>
+      <ButtonDemo>OK</ButtonDemo>
+      <ButtonDemo>Cancel</ButtonDemo>
+    </footer>
+  </div>
+</template>
+<script lang="ts">
+import ButtonDemo from "./ButtonDemo.vue";
+export default {
+  components: {
+    ButtonDemo,
+  },
+};
+</script>
--- src/views/Dialog.vue
+++ src/views/Dialog.vue
@@ -1,1 +1,13 @@
-<template><div>dialog组件</div></template>
+<template>
+  <div>Diolag示例</div>
+  <h1>示例1</h1>
+  <DialogDemo></DialogDemo>
+</template>
+<script lang="ts">
+import DialogDemo from "../lib/DialogDemo.vue";
+export default {
+  components: {
+    DialogDemo,
+  },
+};
+</script>
