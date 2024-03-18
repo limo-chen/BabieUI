eb8bab7931af7da79a3b9101037fa7dd6039afc6
Calista <2320035651@qq.com>
2024-03-18 18:37:34 +0800

    复杂一点的高亮

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -4,17 +4,17 @@
     <div class="content">
       <aside v-if="asideVisible">
         <h2>文档</h2>
-        <ol>
-          <li>
-            <router-link to="/doc/intro">介绍</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/install">安装</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/started">开始使用</router-link>
-          </li>
-        </ol>
+
+        <li>
+          <router-link to="/doc/intro">介绍</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/install">安装</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/started">开始使用</router-link>
+        </li>
+
         <h2>组件列表</h2>
         <li>
           <router-link to="/doc/switch">Switch组件</router-link>
@@ -73,9 +73,9 @@ export default {
   }
 }
 aside {
-  background: #81b6f8;
+  background: #ffc8db;
   width: 150px;
-  padding: 16px;
+  padding: 16px 0;
   position: fixed;
   top: 0;
   left: 0;
@@ -83,10 +83,18 @@ aside {
   height: 100%;
   > h2 {
     margin-bottom: 4px;
+    padding: 0 16px;
   }
 
   > li {
-    padding: 4px 0;
+    > a {
+      display: block;
+
+      padding: 4px 16px;
+    }
+    .router-link-active {
+      background: white;
+    }
   }
 }
 main {
