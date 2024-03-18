d36cf7999d86fd783011c8ed1994b459869b6d4b
Calista <2320035651@qq.com>
2024-03-18 18:51:28 +0800

    添加内容样式

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>install</div>
+  <article>
+    <h1>安装</h1>
+    <p>打开终端运行以下命令：</p>
+    <pre><code>npm install Barbie-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add Barbie-ui</code></pre>
+
+    <p>
+      下一节：<a href="#/doc/get-started" style="color: #fd4973">开始使用</a>
+    </p>
+  </article>
 </template>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>introduce</div>
+  <article>
+    <h2>介绍</h2>
+    <p>Barbie UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <p>
+      这款组件库其实是我为了总结自己这几年的技术经验而写的，全程亲手编写，尽量不采用第三方库，包括你现在看到的这个官网也几乎都是我自己写的。
+    </p>
+    <p>
+      所以如果强烈不建议你将此 UI
+      库用于生产环境。但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的。源代码放在了
+      https://github.com/limo-chen/BabieUI，历史提交信息非常规范，你可以按提交的顺序逐个查看；你也可以直接查看每个组件的源代码和示例，运行方法见
+      README.md。
+    </p>
+    <p>
+      并且我写了对应的项目记录，语雀地址为：https://www.yuque.com/dongrufengfengtu/xgq83l/gu5i0k
+    </p>
+    下一节：<a href="#/doc/install" style="color: #fd4973">安装</a>
+  </article>
 </template>
